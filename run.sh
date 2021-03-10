7f6adeb65d03e5bdcd8d00f2f0627ccf218ebc8f
romka <8026787@gmail.com>
2021-03-10 16:23:15 +0300

    port fix

--- run.sh
+++ run.sh
@@ -14,5 +14,5 @@ if docker ps -a | grep -q twake-mobile-server; then
   docker rm -f twake-mobile-server-chat
 
 fi
-docker run -e NODE_ENV=production -e CORE_HOST=https://chat.twake .app -p 80:3123 -p 3123:3123 --name twake-mobile-server-web-qa -d twake-mobile-server
+docker run -e NODE_ENV=production -e CORE_HOST=https://chat.twake.app -p 80:3123 -p 3123:3123 --name twake-mobile-server-web-qa -d twake-mobile-server
 docker run -e NODE_ENV=production -e CORE_HOST=https://web.qa.twake.app -p 3129:3123 --name twake-mobile-server-chat -d twake-mobile-server
